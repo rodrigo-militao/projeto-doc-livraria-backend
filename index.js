b07522506284c09e48df88f292dcfce731dce4be
Weslley Araújo <46850407+wellwelwel@users.noreply.github.com>
2021-11-06 05:32:15 -0300

    Update index.js
    - Como consequência das mudanças dos arquivos `db.js` e `script.sql`, as rotas precisarão ser adaptadas igualmente.

--- index.js
+++ index.js
@@ -1,74 +1,57 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const db = require("./db");
+(() => {
+   const express = require('express');
+   const app = express();
+   const bodyParser = require('body-parser');
+   const db = require('./db');
+   const cors = require('cors');
+   const PORT = 5001;
 
-const cors = require('cors');
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
+   app.use(cors({ origin: 'http://localhost:3000' }));
 
-app.use(bodyParser.json())
+   app.use(bodyParser.json());
 
-const PORT = 5001
+   /*
 
-app.get('/', (req, res) => {
-    db.findAllArticles()
-        .then(articles => {
-            res.json(articles)
-        })
-        .catch(err => {
-            res.status(500).json({ error: err.message })
-        })
-});
+   app.get('/', (req, res) => {
+      db.findAllArticles().
+         then(articles => res.json(articles)).
+         catch(err => res.status(500).json({ error: err.message }));
+   });
 
-app.get('/:id', (req, res) => {
-  const { id } = req.params
-  db.findArticle(id)
-      .then(article => {
-          res.json(article)
-      })
-      .catch(err => {
-          res.status(500).json({ error: err.message })
-      })
-});
+   app.get('/:id', (req, res) => {
+      const { id } = req.params;
 
-app.post('/', (req, res) => {
-    const { title, content } = req.body
-    db.createArticle(title, content)
-        .then(article => {
-            res.json(article)
-        })
-        .catch(err => {
-            res.status(500).json({ error: err.message })
-        })
-});
+      db.findArticle(id).
+         then(article => res.json(article)).
+         catch(err => res.status(500).json({ error: err.message }));
+   });
 
-app.put('/:id', (req, res) => {
-  const { title, content } = req.body
-  const { id } = req.params
-  db.updateArticle(id, {title, content})
-    .then(article => {
-      res.json(article)
-    })
-    .catch(err => {
-      res.status(500).json({ error: err.message })
-    })
-});
+   app.post('/', (req, res) => {
+      const { title, content } = req.body;
 
-app.delete('/:id', (req, res) => {
-    const { id } = req.params
-    db.deleteArticle(id)
-        .then(() => {
-            res.json({ message: 'Article deleted' })
-        })
-        .catch(err => {
-            res.status(500).json({ error: err.message })
-        })
-});
+      db.createArticle(title, content).
+         then(article => res.json(article)).
+         catch(err => res.status(500).json({ error: err.message }));
+   });
 
+   app.put('/:id', (req, res) => {
+      const { title, content } = req.body;
+      const { id } = req.params;
 
+      db.updateArticle(id, {title, content}).
+         then(article => res.json(article)).
+         catch(err => res.status(500).json({ error: err.message }));
+   });
 
-app.listen(PORT, () => {
-    console.log('Server is running on port ' + PORT);
-});
+   app.delete('/:id', (req, res) => {
+      const { id } = req.params;
+
+      db.deleteArticle(id).
+         then(() => res.json({ message: 'Article deleted' })).
+         catch(err => res.status(500).json({ error: err.message }));
+   });
+
+   */
+
+   app.listen(PORT, () => console.log('Server is running on port ' + PORT));
+})();
