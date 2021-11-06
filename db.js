da7935b48ce9b7642f05ad811b15053c8254e346
rodrigo-militao <militaodorigo9@gmail.com>
2021-11-06 10:01:47 -0300

    config no banco local pegar artigo pelo postId gerador de PostId aleatorio Consideracoes sobre estrutura sql

--- db.js
+++ db.js
@@ -4,11 +4,11 @@ async function connect(){
  
     const mysql = require("mysql2/promise");
 
-    const db_user = "carlisson"
+    const db_user = "root"
     const db_password = "12345678"
-    const db_host = "db4free.net"
+    const db_host = "localhost"
     const db_port = 3306
-    const db_name = "robo_trade"
+    const db_name = "main"
 
     const connection = await mysql.createConnection(`mysql://${db_user}:${db_password}@${db_host}:${db_port}/${db_name}`);
     console.log("Conectou no MySQL!");
@@ -16,9 +16,9 @@ async function connect(){
     return connection;
 }
 
-async function findArticle(id){
+async function findArticle(postId){
     const connection = await connect();
-    const [rows] = await connection.query(`SELECT * FROM articles WHERE id = ?`, [id]);
+    const [rows] = await connection.query(`SELECT * FROM articles WHERE postId = ?`, [postId]);
     return rows[0];
 }
 
@@ -29,8 +29,9 @@ async function findAllArticles(){
 }
 
 async function createArticle(title, content){
-    const connection = await connect();
-    const [rows] = await connection.query(`INSERT INTO articles (title, content) VALUES (?, ?)`, [title, content]);
+    const connection = await connect()
+    const postId = makePostId(10)
+    const [rows] = await connection.query(`INSERT INTO articles (postId, title, content) VALUES (?, ?, ?)`, [postId, title, content]);
     return rows;
 }
 
@@ -44,6 +45,17 @@ async function deleteArticle(id){
     const connection = await connect();
     const [rows] = await connection.query(`DELETE FROM articles WHERE id = ?`, [id]);
     return rows;
+}
+
+function makePostId(number_of_chars) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < number_of_chars; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
 }
 
 module.exports = {
