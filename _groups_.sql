8f8e0fc1ab7d6d6caff2fadcba375777f9ec2a8e
rodrigo-militao <militaodorigo9@gmail.com>
2021-11-13 18:53:33 -0300

    Inclusao de arquivos backup

---
+++ _groups_.sql
@@ -0,0 +1,4 @@
+INSERT INTO main.`groups` (title,`order`,created_by,created_at,updated_by,updated_at) VALUES
+	 ('Principal',0,NULL,'2021-11-06 23:50:18',NULL,'2021-11-06 23:50:18'),
+	 ('Segundo',1,NULL,'2021-11-06 23:50:45',NULL,'2021-11-06 23:50:45'),
+	 ('admin',0,NULL,'2021-11-09 18:37:18',NULL,'2021-11-09 18:37:18');
